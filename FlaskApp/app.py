8d89a592f6f0eb945b13d17ba2e880ade2b1a0c3
jmcquown <jmcq521@gmail.com>
2017-04-01 23:57:00 -0400

    main()

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, session, redirect
+from flask.ext.pymongo import PyMongo
+import bcrypt
+
 app = Flask(__name__)
+
+mongo = PyMongo(app)
 
 @app.route("/")
 def main():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
 	return render_template('index.html')
 
 @app.route("/sign_in")
