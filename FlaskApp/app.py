fea3531ddbf86c2a1b8b1c12d2dcf1a67647f41e
jmcquown <jmcq521@gmail.com>
2017-04-02 01:35:32 -0400

    typos probs

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, session, redirect
-from flask.ext.pymongo import PyMongo
+from flask_pymongo import PyMongo
 import bcrypt
 
 app = Flask(__name__)
@@ -11,21 +11,26 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    if 'username' in session:
-        return 'You are logged in as ' + session['username']
+    # if 'username' in session:
+        # return "fuck"
+        # return 'You are logged in as ' + session['username']
 	return render_template('index.html')
 
-@app.route("/sign_in", methods=['POST'])
+@app.route("/sign_in")
 def sign_in():
+    return render_template('sign_in.html')
+
+@app.route("/login", methods=['POST'])
+def login():
     users = mongo.db.users
-    login_user = user.find_one({'name' : request.form['username']})
+    login_user = users.find_one({'name' : request.form['username']})
 
     if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].enconde('utf-8')) == ogin_user['password'].encode('utf-8'):
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].enconde('utf-8')) == login_user['password'].encode('utf-8'):
             session['username'] = request.form['username']
             return redirect(url_for('index'))
 
-	return 'Invalid username/password combination'
+    return 'Invalid username/password combination'
 
 @app.route("/view_pets")
 def view_pets():
