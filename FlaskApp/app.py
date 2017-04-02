8ccdc747406308230ffbf00ffda16e8725e1bd0b
jmcquown <jmcq521@gmail.com>
2017-04-02 03:30:07 -0400

    URI not URL :sleeping:

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -6,7 +6,7 @@ import bcrypt
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'hack-princeton'
-app.config['MONGO_URL'] = 'mongodb://hack:princeton@ds149040.mlab.com:49040/hack-princeton'
+app.config['MONGO_URI'] = 'mongodb://hack:princeton@ds149040.mlab.com:49040/hack-princeton'
 
 mongo = PyMongo(app)
 
@@ -29,7 +29,7 @@ def login():
     login_user = users.find_one({'name' : request.form['username']})
 
     if login_user:
-        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].enconde('utf-8')) == login_user['password'].encode('utf-8'):
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
             session['username'] = request.form['username']
             return redirect(url_for('index'))
     flash(Markup("Invalid username/password"))
@@ -50,6 +50,7 @@ def sign_up():
             users.insert({'name' : request.form['InputEmail'], 'password' : hashpass, 'street' : request.form['InputStreetAddress'], 'city' : request.form['InputCityAddress'], 'zip' : request.form['InputZipcode'], 'phone' : request.form['InputPhone']})
             session['username'] = request.form['InputEmail']
             return redirect(url_for('index'))
+        return "Username already exists!"    
 
     return render_template('sign_up.html')
 
